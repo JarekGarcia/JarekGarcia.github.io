f7a3d95b0a25e18684cc80478923c796f8122169
JarekGarcia <145396728+JarekGarcia@users.noreply.github.com>
2024-01-19 13:29:33 -0700

    Delete assets directory

--- assets/TestHandler-blsCTpjU.js
+++
@@ -1,1 +0,0 @@
-import{S as s}from"./index-22G9LAjs.js";class t extends s{constructor(){super(),this.on("IS_TESTED",this.onTest)}onTest(e){console.log("Recieved IS_TESTED",e)}}const r=new t;export{r as testHandler};
